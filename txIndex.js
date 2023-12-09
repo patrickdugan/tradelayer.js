19d31f198659e931be681ca0952289236189d6db
Patrick Dugan <patrick.buchanan.dugan@gmail.com>
2023-12-09 11:56:20 -0300

    Revived txIndex function post refactor

--- txIndex.js
+++ txIndex.js
@@ -3,7 +3,7 @@ const json = require('big-json');
 const util = require('util');
 const txUtils = require('./txUtils');
 const Types = require('./types.js');
-const { txIndexDB } = require('./db.js');
+const { ensureDbOpen, txIndexDB } = require('./db.js');
 
 class TxIndex {
     static instance;
@@ -13,19 +13,19 @@ class TxIndex {
             return TxIndex.instance;
         }
 
-        const clientConfig = test ? {
+        const clientConfig = /*test ?*/ {
             host: '127.0.0.1',
             port: 18332,
             user: 'user',
             pass: 'pass',
             timeout: 10000
-        } : {
+        } /*: {
             host: '127.0.0.1',
             port: 8332,
             user: 'user',
             pass: 'pass',
             timeout: 10000
-        };
+        };*/
 
         this.client = new litecoin.Client(clientConfig);
         this.decoderawtransactionAsync = util.promisify(this.client.cmd.bind(this.client, 'decoderawtransaction'));
@@ -43,6 +43,7 @@ class TxIndex {
     }
 
     static async initializeIndex(genesisBlock) {
+        await ensureDbOpen(); // Ensure the database is open
         await txIndexDB.put('genesisBlock', genesisBlock);
         await txIndexDB.put('indexExists', true);
         await TxIndex.getInstance().extractBlockData(genesisBlock);
@@ -51,7 +52,9 @@ class TxIndex {
     async extractBlockData(startHeight) {
         let chainTip = await this.fetchChainTip();
         for (let height = startHeight; height <= chainTip; height++) {
+            //console.log(height)
             let blockData = await this.fetchBlockData(height);
+            //console.log(blockData)
             await this.processBlockData(blockData, height);
             chainTip = await this.fetchChainTip();
         }
@@ -92,14 +95,15 @@ class TxIndex {
         for (const txId of blockData.tx) {
             const txHex = await this.fetchTransactionData(txId);   
             const txData = await this.DecodeRawTransaction(txHex);
-            
+            //console.log(txData)
             if(txData != null){
+                //console.log(txData.marker)
                 if (txData.marker === 'tl') {
+                    console.log(txData.payload)
                     this.transparentIndex.push(txData.payload);
                     const txDetails = await this.processTransaction(txData.payload, txData.decodedTx, txId, txData.marker);
                     await this.saveTransactionData(txId, txData.decodedTx, txData.payload, blockHeight, txDetails);
                     console.log(txDetails);
-                    await this.saveTransactionByHeight(txId, blockHeight);
                 }
             }
         }
@@ -118,13 +122,34 @@ class TxIndex {
         });
     }
 
-    async DecodeRawTransaction(rawTx) {
+     async DecodeRawTransaction(rawTx) {
         try {
             const decodedTx = await this.decoderawtransactionAsync(rawTx);
+            
+            if(rawTx=="02000000000101be64c98a4c17b5861b45b2602873212cb0ada374539c7b61593b2d3e47b8e5cd0100000000ffffffff020000000000000000066a04746c303080b9ff0600000000160014ebecd536259ef21bc6ecc18e45b35412f04722900247304402201ac4b0e373e7555d502e80b5424683dd1da2ca8052793bd2c62d64b2e9370367022014e72b32507262b3c78848b81558acfb2c9f9cb2a8c7968b65615888e7f04d0b012103d6521aea309f7a2768a1cabcb917664966cabc28bc23874b12f73c1989972c5f00000000"){
+                console.log('Decoded Transaction:', decodedTx);
+            }
+            const opReturnOutput = decodedTx.vout.find(output => output.scriptPubKey.type === 'nulldata');
+            if (opReturnOutput) {
+                const opReturnData = opReturnOutput.scriptPubKey.hex;
+                //console.log('OP_RETURN Data:', opReturnData);
+                // Extract and log the "tl" marker
+                const markerHex = opReturnData.substring(4, 8); // '746c' for 'tl'
+                const marker = Buffer.from(markerHex, 'hex').toString();
+                if(marker=='tl'){console.log('Marker:', marker);}
+                // Extract and log the actual payload
+                const payloadHex = opReturnData.substring(8);
+                const payload = Buffer.from(payloadHex, 'hex').toString();
+                if(marker=='tl'){console.log('Decoded Payload:', payload)};
+                return { marker, payload , decodedTx};
+            } else {
+                //console.log('No OP_RETURN output found.');
+                return null;
+            }
             // Process decoded transaction logic here...
             return decodedTx;
         } catch (error) {
-            console.error('Error decoding raw transaction:', error);
+            //console.error('Error decoding raw transaction:', error);
         }
     }
 
@@ -136,29 +161,12 @@ class TxIndex {
         return { sender, reference, payload, decodedParams };
     }
 
-    static async saveTransactionByHeight(txId, blockHeight) {
-        try {
-            const txKey = `txHeight-${blockHeight}-${txId}`;
-            const instance = TxIndex.getInstance();
-            const txData = await instance.fetchTransactionData(txId);
-            console.log(`Saving transaction data for key ${txKey}:`, txData);
-            if (txData) {
-                await txIndexDB.put(txKey, JSON.stringify(txData));
-                console.log(`Transaction data saved successfully for key ${txKey}`);
-            } else {
-                console.log(`No transaction data found for txId ${txId}`);
-            }
-        } catch (error) {
-            console.error(`Error in saveTransactionByHeight for txId ${txId}:`, error);
-        }
-    }
-
-    static async saveTransactionData(txId, txData, payload, blockHeight) {
+    async saveTransactionData(txId, txData, payload, blockHeight) {
         const indexKey = `tx-${blockHeight}-${txId}`;
         await txIndexDB.put(indexKey, JSON.stringify({ txData, payload }));
     }
 
-    static async loadIndex() {
+    async loadIndex() {
         return new Promise((resolve, reject) => {
             let data = {};
             txIndexDB.createReadStream()
