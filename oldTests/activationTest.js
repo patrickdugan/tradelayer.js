39da581dc6a9096dc16b16db43e82fc5770894c4
Valentin Peshev <dev.peshev@gmail.com>
2024-04-03 07:53:43 +0300

    remove test and db files

--- oldTests/activationTest.js
+++
@@ -1,6 +0,0 @@
-var testAdmin = "tltc1qa0kd2d39nmeph3hvcx8ytv65ztcywg5sazhtw8"
-
-// Example usage
-const tokenData = { /* token details */ };
-const tokenCreationMessage = await createAndRegisterToken(tokenData);
-console.log(tokenCreationMessage);
