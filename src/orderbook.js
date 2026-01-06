bc07e0455ef93f6533fa160fcdbac8e812da806a
Patrick Dugan <patrick.buchanan.dugan@gmail.com>
2026-01-06 15:03:55 -0300

    more solid price fetch in process contracts

--- src/orderbook.js
+++ src/orderbook.js
@@ -2244,8 +2244,10 @@ class Orderbook {
                     // Load the margin map for the given series ID and block height
                     const marginMap = await MarginMap.loadMarginMap(match.sellOrder.contractId);
                     const isInverse = await ContractRegistry.isInverse(match.sellOrder.contractId)
-                    let lastPrice = await ContractRegistry.getPriceAtBlock(match.sellOrder.contractId,currentBlockHeight-1)
+                    const priceInfo = await Clearing.isPriceUpdatedForBlockHeight(id, blockHeight);
+                    const lastPrice = priceInfo.lastPrice
                     if(isLiquidation&&last!=null){lastPrice=last}
+                        console.log('last price in process contracts '+lastPrice)
                     match.inverse = isInverse
                     let collateralPropertyId = await ContractRegistry.getCollateralId(match.buyOrder.contractId)
                     const blob = await ContractRegistry.getNotionalValue(match.sellOrder.contractId,match.tradePrice)
